9738777258e7d37f498a43c6dc004e11d1cb893c
felipeflourwears <A01274880@tec.mx>
2023-09-14 17:20:39 -0600

    Update

--- imprimir_ticket.php
+++ imprimir_ticket.php
@@ -5,37 +5,52 @@ use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
 use Mike42\Escpos\Printer;
 use Mike42\Escpos\EscposImage;
 
+ // Establece el nombre de la impresora que deseas usar
+ $printer_name = 'POS'; // Reemplaza con el nombre de tu impresora
+
+ // Crea una instancia de la clase WindowsPrintConnector
+ $connector = new WindowsPrintConnector($printer_name);
+
+ // Crea una instancia de la clase Printer
+ $printer = new Printer($connector);
+
 try {
-    // Establece el nombre de la impresora que deseas usar
-    $printer_name = 'POS'; // Reemplaza con el nombre de tu impresora
+   
+    $tux = EscposImage::load("coca.jpg", false);
+    $printer -> bitImage($tux);
 
-
-    // Crea una instancia de la clase WindowsPrintConnector
-    $connector = new WindowsPrintConnector($printer_name);
-
-    // Crea una instancia de la clase Printer
-    $printer = new Printer($connector);
+   
 
     $printer->setJustification(Printer::JUSTIFY_CENTER);
 
-
     $printer->setTextSize(2, 2);
-    $printer->text("Ticket con PHP");
-
+    $printer->text("Coca Cola\n");
+    $printer->setTextSize(1, 1);
     $printer->text("¡Felicidades!\n");
-    $printer->text("Has completado el juego de memorama.\n");
+    $printer->text("Disfruta de esta increible promocion.\n");
+    $printer->text("Presenta este ticket en caja y recibe 20% de descuento en tu compra.\n");
+    $printer->text("\n");
+    $printer->text("\n");
+    
+    $printer -> bitImage($tux, Printer::IMG_DOUBLE_WIDTH | Printer::IMG_DOUBLE_HEIGHT);
+    $printer -> text("Large Tux in correct proportion (bit image).\n");
     
 
 
-    // Corta el papel (si tu impresora lo soporta)
-    $printer->cut();
-
-    // Cierra la conexión con la impresora
-    $printer->close();
+   
     
     echo "Impresión exitosa.";
 
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
+
 }
+
+ // Corta el papel (si tu impresora lo soporta)
+ $printer->cut();
+
+ // Cierra la conexión con la impresora
+ $printer->close();
 ?>
+
+
