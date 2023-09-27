f4cecd18823879918dca7732c003665e7533997f
felipeflourwears <A01274880@tec.mx>
2023-09-27 11:21:23 -0600

    Update Ticket

--- imprimir_ticket.php
+++ imprimir_ticket.php
@@ -10,7 +10,7 @@ use Mike42\Escpos\EscposImage;
 
 try {
     // Enter the device file for your USB printer here
-    $connector = new FilePrintConnector("/dev/usb/lp1");
+    $connector = new FilePrintConnector("/dev/usb/lp0");
 
     /* Print a receipt */
     $printer = new Printer($connector);
@@ -18,15 +18,33 @@ try {
     //$printer -> graphics($tux);
 
     $img = EscposImage::load("coca.jpg", false);
-
+    $testStr = "https://www.coca-cola.com.mx/";
     // Imprime la imagen utilizando bitImage
     $printer->bitImage($img);
 
     $printer->setJustification(Printer::JUSTIFY_CENTER);
     $printer->setTextSize(2, 2);
-    $printer -> text("Disfruta de esta increíble promoción\n");
+    $printer -> text("Disfruta de esta increible promocion\n");
     $printer -> text("Presenta este ticket en caja y recibe 20% de descuento en tu compra\n");
     $printer -> feed();
+    // Aumenta el tamaño del código QR (por ejemplo, a 8)
+    $printer -> qrCode($testStr, Printer::QR_ECLEVEL_L, 8);
+    $printer -> text("\n");
+    // Configura el tamaño del código de barras y la posición del texto
+    $printer->setBarcodeHeight(80);
+    $printer->setBarcodeWidth(2);
+    $printer->setBarcodeTextPosition(Printer::BARCODE_TEXT_BELOW);
+
+    // Imprime el código de barras
+    $printer->barcode("123456789", Printer::BARCODE_CODE39); // Cambia el contenido a tu código de barras deseado
+    $printer -> text("\n");
+    $printer -> text("\n");
+    // Corta el papel
+    $printer->cut();
+
+    echo "Impresión exitosa.";
+
+    
 
 
     /* Close printer */
